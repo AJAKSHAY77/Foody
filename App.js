fae2b7aad8439807e6b603bb6b6e8fafbb1c1637
akshay77 <ajakshayjain11@gmail.com>
2023-12-09 17:51:31 +0530

    install react

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
-import react from "react"
-import reactDOM from "react-dom/client"
+import react from "react";
+import reactDOM from "react-dom/client";
 
-const root = reactDOM.createRoot(document.getElementById("root"))
-root.render()
+const heading = react.createElement("h1", { id: "heading" }, "akshay jain");
+
+const heading2 = <h1>hello all</h1>;
+const Compo = () => {
+    return (
+        <p>hello all public</p>
+    )
+}
+
+
+const root = reactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Compo/>);
